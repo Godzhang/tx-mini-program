fa355191a3bc0335bac128f8acecdccfb1ccd0ab
godzhang <zhangqijilindaxue@126.com>
2024-05-07 18:39:18 +0800

    feat: 添加组件库nutui

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,24 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index'
-  ],
+  pages: ["pages/index/index", "pages/my/index"],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+  // tabBar: {
+  //   color: "#999",
+  //   selectedColor: "#333",
+  //   backgroundColor: "#fff",
+  //   list: [
+  //     {
+  //       pagePath: "pages/index/index",
+  //       text: "首页",
+  //     },
+  //     {
+  //       pagePath: "pages/my/index",
+  //       text: "我的",
+  //     },
+  //   ],
+  // },
+});
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,20 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { View, Text } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import { Button } from "@nutui/nutui-react-taro";
+import { Plus } from "@nutui/icons-react-taro";
+import "./index.scss";
 
 export default function Index() {
-
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log("Page loaded.");
+  });
 
   return (
-    <View className='index'>
-      <Text>Hello world!</Text>
+    <View className="container">
+      <View className="task-list"></View>
+      <View className="task-input">
+        <Button type="primary" icon={<Plus size="20" />} />
+      </View>
     </View>
-  )
+  );
 }
